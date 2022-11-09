85a088374fdf392cbfa0a4fbba4cbc6583bfee2c
Dilshod Nazarov <tegeran21@gmail.com>
2022-11-09 12:51:06 +0500

    My First Commmit

---
+++ src/main/java/com/darktroll/plugin/Main.java
@@ -0,0 +1,23 @@
+package com.darktroll.plugin;
+
+import org.bukkit.block.Sign;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
